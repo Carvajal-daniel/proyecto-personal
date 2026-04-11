2e64ec1f4c6f02d67baad58c3e69bc2ab2b03559
Carvajal-Daniel <carvajal.dev@hotmail.com>
2026-04-11 16:49:51 -0300

    started heeader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Header from "@/common/header/header";
 
 export default function Home() {
   return (
-    <div>
-      <h1>texto</h1>
-    </div>
+   <header>
+    <Header />
+   </header>
   );
 }
