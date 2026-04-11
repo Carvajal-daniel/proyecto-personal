24963097d771eab639ce276b58b95ae27c06698c
Carvajal-Daniel <carvajal.dev@hotmail.com>
2026-04-11 19:24:33 -0300

    header updated

--- src/components/header/LoginButton.tsx
+++ src/components/header/LoginButton.tsx
@@ -1,10 +1,9 @@
 import Button from "../ui/Button"
-import Gradient from "./Gradient"
 
 const LoginButton = () => {
   return (
  <div className="relative inline-flex items-center">
-  <Gradient />
+
   <Button className="bg-gray-900 tracking-wider text-[12.5px] shadow-md shadow-gray-800 ">Entrar</Button>
 </div>
   )
