0dd0811de1901914bacbe59d74995093e78ef88a
Carvajal-Daniel <carvajal.dev@hotmail.com>
2026-04-11 19:25:37 -0300

    header shadow changed

--- src/components/header/LoginButton.tsx
+++ src/components/header/LoginButton.tsx
@@ -4,7 +4,7 @@ const LoginButton = () => {
   return (
  <div className="relative inline-flex items-center">
 
-  <Button className="bg-gray-900 tracking-wider text-[12.5px] shadow-md shadow-gray-800 ">Entrar</Button>
+  <Button className="bg-gray-900 tracking-wider text-[12.5px] shadow-sm shadow-gray-800 ">Entrar</Button>
 </div>
   )
 }
