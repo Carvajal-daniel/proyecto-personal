10464947cbc68fff33b56fef1c0aa51bc4887761
Carvajal-Daniel <carvajal.dev@hotmail.com>
2026-04-11 18:35:34 -0300

    header updated

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,7 +6,7 @@ import Button from "./LoginButton"
 const Header = () => {
   return (
     <header className="w-full fixed top-0 z-50 backdrop-blur-md bg-white/70 border-b border-gray-200 shadow-lg">
-      <div className="max-w-7xl mx-auto px-6 py-2 flex items-center justify-between">
+      <div className="max-w-7xl mx-auto px-2 py-2 flex items-center justify-between">
 
         {/* Logo */}
         <Link href="/" className="flex items-center gap-2">
